ed05e033c39a84549756110ad5d1af27ab24ff97
VladT <v.tertichniy@epages.in.ua>
2020-12-07 22:39:15 +0200

    dddd

--- traffic.php
+++ traffic.php
@@ -63,4 +63,4 @@ else{//если файл существует
     $smarty->assign('mas_traffic', $mas_traffic);//передача массива в шаблонизатор для вывода
 }
 
-$smarty->display("$path/temp/smarty/templates/traffic.tpl");//вызов отображения шаблона
+$smarty->display("temp/smarty/templates/traffic.tpl");//вызов отображения шаблона
